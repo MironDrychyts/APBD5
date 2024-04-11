43bef21a7723dad5f95a128ca4d177c7e98f5ff2
MironDrychyts <s24113@pjwstk.edu.pl>
2024-04-11 20:18:10 +0200

    Commit2

--- APBD/obj/Debug/net8.0/Tutorial4.AssemblyInfo.cs
+++ APBD/obj/Debug/net8.0/Tutorial4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91245c7ff4e7f97ed1be69f139f50dc9b539af27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
